d4750acc8be20f2323fca78fb6f6c037746ad230
kimkihong <ganzik83@gmail.com>
2020-04-21 17:28:04 +0900

    exif 정보

--- android/src/main/java/com/syanpicker/RNSyanImagePickerModule.java
+++ android/src/main/java/com/syanpicker/RNSyanImagePickerModule.java
@@ -377,6 +377,35 @@ public class RNSyanImagePickerModule extends ReactContextBaseJavaModule {
         invokeSuccessWithResult(imageList);
     }
 
+//     ExifInterface
+     private static final String[] EXIF_ATTRIBUTES = new String[] {
+            ExifInterface.TAG_APERTURE,
+            ExifInterface.TAG_DATETIME,
+            ExifInterface.TAG_DATETIME_DIGITIZED,
+            ExifInterface.TAG_EXPOSURE_TIME,
+            ExifInterface.TAG_FLASH,
+            ExifInterface.TAG_FOCAL_LENGTH,
+            ExifInterface.TAG_GPS_ALTITUDE,
+            ExifInterface.TAG_GPS_ALTITUDE_REF,
+            ExifInterface.TAG_GPS_DATESTAMP,
+            ExifInterface.TAG_GPS_LATITUDE,
+            ExifInterface.TAG_GPS_LATITUDE_REF,
+            ExifInterface.TAG_GPS_LONGITUDE,
+            ExifInterface.TAG_GPS_LONGITUDE_REF,
+            ExifInterface.TAG_GPS_PROCESSING_METHOD,
+            ExifInterface.TAG_GPS_TIMESTAMP,
+            ExifInterface.TAG_IMAGE_LENGTH,
+            ExifInterface.TAG_IMAGE_WIDTH,
+            ExifInterface.TAG_ISO,
+            ExifInterface.TAG_MAKE,
+            ExifInterface.TAG_MODEL,
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.TAG_SUBSEC_TIME,
+            ExifInterface.TAG_SUBSEC_TIME_DIG,
+            ExifInterface.TAG_SUBSEC_TIME_ORIG,
+            ExifInterface.TAG_WHITE_BALANCE
+    };
+    
     private WritableMap getImageResult(LocalMedia media, Boolean enableBase64) {
         WritableMap imageMap = new WritableNativeMap();
         String path = media.getPath();
@@ -387,6 +416,18 @@ public class RNSyanImagePickerModule extends ReactContextBaseJavaModule {
 
         if (media.isCut()) {
             path = media.getCutPath();
+        }
+        
+          try {
+            ExifInterface exif = new ExifInterface(path);
+
+            for (String attribute : EXIF_ATTRIBUTES) {
+                String value = exif.getAttribute(attribute);
+                imageMap.putString(attribute, value);
+            }
+
+        } catch (Exception e) {
+            Log.e("error", e.toString());
         }
 
         BitmapFactory.Options options = new BitmapFactory.Options();
