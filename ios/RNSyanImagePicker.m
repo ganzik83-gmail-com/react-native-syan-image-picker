47032405c532508e528be8f8797981607e9c2b83
kimkihong <ganzik83@gmail.com>
2020-04-23 22:56:43 +0900

    ios 크롭사진 확장자 출력

--- ios/RNSyanImagePicker.m
+++ ios/RNSyanImagePicker.m
@@ -465,6 +465,7 @@ RCT_EXPORT_METHOD(openVideoPicker:(NSDictionary *)options callback:(RCTResponseS
     NSInteger size = [[NSFileManager defaultManager] attributesOfItemAtPath:filePath error:nil].fileSize;
     photo[@"size"] = @(size);
     photo[@"mediaType"] = @(phAsset.mediaType);
+	photo[@"fileExtension"] = fileExtension;
     if ([self.cameraOptions sy_boolForKey:@"enableBase64"]) {
         photo[@"base64"] = [NSString stringWithFormat:@"data:image/jpeg;base64,%@", [writeData base64EncodedStringWithOptions:0]];
     }
